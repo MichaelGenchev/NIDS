458fffde5cd8d2e5af74654d1574bd2c39dd4bd5
MichaelGenchev <89229268+MichaelGenchev@users.noreply.github.com>
2023-01-20 14:53:15 +0200

    added makefile

---
+++ initdb/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var uri = flag.String("uri", "mongodb://localhost:27017", "MongoDB URI")
+func main() {
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(*uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(context.TODO())
+	// Create collections
+	collections := []string{"alerts", "parsedPackets", "signatures"}
+	for _, collectionName := range collections {
+		client.Database("NIDS").CreateCollection(context.TODO(), collectionName)
+	}
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+init-db:
+	go run initdb/main.go --uri=$(uri)
