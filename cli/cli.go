32d5970d42c930680887e4d4bc5de2b36baafd3e
MichaelGenchev <89229268+MichaelGenchev@users.noreply.github.com>
2023-01-20 15:28:26 +0200

    added error handling in the CLI interface

--- cli/cli.go
+++ cli/cli.go
@@ -4,9 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/fatih/color"
+	"github.com/google/gopacket/pcap"
 )
 
 // Define flags for command-line arguments
@@ -32,21 +35,53 @@ func printWelcome() {
 
 // getNetworkInterface function to ask the user for a network interface
 func getNetworkInterface() string {
+	// Find all available network interfaces
+	interfaces, _ := pcap.FindAllDevs()
+	interfacesDict := make(map[int]string)
+
+	// Print the name of each interface
+	for indx, intf := range interfaces {
+		interfacesDict[indx + 1] = intf.Name
+	}
 	reader := bufio.NewReader(os.Stdin)
 	color.Cyan("Enter the network interface you want to use: ")
+	for key, val := range interfaces {
+		color.Red("%d. - %s", key+1, val.Name)
+	}
 	interfaceFlag, _ := reader.ReadString('\n')
 	interfaceFlag = strings.TrimSpace(interfaceFlag)
-	return interfaceFlag
+	
+	number, err := strconv.Atoi(interfaceFlag)
+	if err != nil {
+		return getNetworkInterface()
+	}
+	// check if interfaceFlag is string
+	intf, ok := interfacesDict[number]
+	if !ok {
+		return getNetworkInterface()
+	}
+	return intf
 }
-
+const mongoURIPattern = `^mongodb:\/\/([A-Za-z0-9-_]+):?([A-Za-z0-9-_]+)?@([A-Za-z0-9-_]+):?([0-9]+)?\/([A-Za-z0-9-_]+)?\??([A-Za-z0-9-_=&]+)?$`
 // getMongoURI function to ask the user for the MongoDB URI
 func getMongoURI() string {
 	reader := bufio.NewReader(os.Stdin)
 	color.Cyan("Enter the MongoDB URI: ")
-	mongoURI, _ := reader.ReadString('\n')
+	mongoURI, err := reader.ReadString('\n')
+	if err != nil {
+		color.Cyan("Error reading the URI. Try again.")
+		return getMongoURI()
+	}
 	mongoURI = strings.TrimSpace(mongoURI)
 	if mongoURI == "" {
 		mongoURI = "mongodb://localhost:27017"
+		return mongoURI
+	}
+	matched, _ := regexp.MatchString(mongoURIPattern, mongoURI)
+	if !matched{
+		color.Cyan("Wrong URI Pattern")
+		return getMongoURI()
+
 	}
 	return mongoURI
 }
@@ -59,7 +94,7 @@ func ParseFlags() *CLI {
 	return &CLI{
 		InterfaceFlag: interfaceFlag,
 		MongoURI:      mongoURI,
-		Channel:       make(chan Info, 100),
+		Channel:       make(chan Info, 10000),
 	}
 }
 func (cli *CLI) AcceptInfo() {
